170e7dec808eba8e66e5782d825d5de14a86f4e6
patricio tula <tula.patricio@gmail.com>
2021-06-27 14:58:25 +0200

    modify some tests

--- tests/unit/services.py/test_web_crawler_service.py
+++ tests/unit/services.py/test_web_crawler_service.py
@@ -1,7 +1,7 @@
 import pytest
 
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 
 from web_crawler.services.web_crawler_service import WebCrawlerService
@@ -10,21 +10,16 @@ from web_crawler.services.web_crawler_service import WebCrawlerService
 @pytest.mark.unit_tests
 class TestWebCrawlerService(TestCase):
 
+    @patch('web_crawler.services.web_crawler_service.BeautifulSoup')
     @patch('web_crawler.services.web_crawler_service.requests.get')
-    def test_get_embedded_web(self, mock_get_html):
+    def test_get_embedded_web(self, mock_get_html, mock_beautifulsoup):
         # Given
-        mock_get_html.return_value.text = """<html><head><title>The Dormouse's story</title></head>
-                                                <body>
-                                                    <p class="title"><b>The Dormouse's story</b></p>
-                                                    <p class="story">Once upon a time there were three little sisters; and their names were
-                                                    <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-                                                    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-                                                    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-                                                    and they lived at the bottom of a well.</p>
-                                                    <p class="story">...</p>
-                                          """
+        mock_beautifulsoup.return_value.find_all.return_value = [MagicMock(**{'get.return_value': 'http://example.com/elsie'}),
+                                                                 MagicMock(**{'get.return_value': 'http://example.com/lacie'}),
+                                                                 MagicMock(**{'get.return_value': 'http://example.com/tillie'})]
+
         # When
-        webs = WebCrawlerService._get_embedded_web('https://google.com')
+        webs = WebCrawlerService._get_embedded_web('https://mock.com')
 
         # Then
         expected_webs = ['http://example.com/elsie',
@@ -32,6 +27,10 @@ class TestWebCrawlerService(TestCase):
                          'http://example.com/tillie']
 
         self.assertEqual(expected_webs, list(webs))
+        mock_get_html.assert_called_once_with('https://mock.com')
+        mock_beautifulsoup.assert_called_once_with(mock_get_html.return_value.text, features='html5lib')
+        mock_beautifulsoup.return_value.find_all.assert_called_with('a')
+
 
     def test_run_unimplemented(self):
         # Given
@@ -41,3 +40,25 @@ class TestWebCrawlerService(TestCase):
         # When / Then
         with self.assertRaises(TypeError):
             FakeWebCrawler()
+
+    @patch('web_crawler.services.web_crawler_service.requests.get')
+    def test_get_embedded_web_requests_raises(self, mock_get_html):
+        # Given
+        mock_get_html.side_effect = Exception('Boom!')
+
+        # When
+        webs = WebCrawlerService._get_embedded_web('https://mock.com')
+
+        # Then
+        self.assertEqual([], list(webs))
+
+    @patch('web_crawler.services.web_crawler_service.BeautifulSoup')
+    def test_get_embedded_web_beautifulsoup_raises(self, mock_beautifulsoup):
+        # Given
+        mock_beautifulsoup.side_effect = Exception('Boom!')
+
+        # When
+        webs = WebCrawlerService._get_embedded_web('https://mock.com')
+
+        # Then
+        self.assertEqual([], list(webs))
